4e0cf4076e46906a36e6e4873b65254a32a5cd26
Pr1yansu <priyansuchowdhury57@gmail.com>
2024-03-13 02:03:23 +0530

    third commit

--- auth.config.ts
+++ auth.config.ts
@@ -3,7 +3,7 @@ import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
 import { NextAuthConfig } from "next-auth";
 import { loginSchema } from "./app/schema/login-schema";
-import prisma from "./lib/prisma";
+import prisma from "@/lib/prisma";
 
 export default {
   providers: [
