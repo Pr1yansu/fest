a3721d597a07c3cf65a3f7b26ec6687025c3c10d
Pr1yansu <priyansuchowdhury57@gmail.com>
2024-03-17 05:45:43 +0530

    loading added

---
+++ app/(frontend)/gemisha/loading.tsx
@@ -0,0 +1,12 @@
+import LoadingAnimation from "@/components/ui/loader";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <>
+      <LoadingAnimation />
+    </>
+  );
+};
+
+export default Loading;
